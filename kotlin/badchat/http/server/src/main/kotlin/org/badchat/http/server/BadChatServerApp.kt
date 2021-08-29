c937c99eceb88c49fbff149cea4217128f709018
Scott Lively <scott@revl.com>
2021-08-29 16:12:49 -0700

    unused imports

--- kotlin/badchat/http/server/src/main/kotlin/org/badchat/http/server/BadChatServerApp.kt
+++ kotlin/badchat/http/server/src/main/kotlin/org/badchat/http/server/BadChatServerApp.kt
@@ -1,16 +1,11 @@
 package org.badchat.http.server
 
-import io.zonky.test.db.postgres.embedded.EmbeddedPostgres
-import org.badchat.messages.ctrl.getLogger
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.boot.web.embedded.undertow.UndertowReactiveWebServerFactory
 import org.springframework.boot.web.reactive.server.ReactiveWebServerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
-import javax.annotation.PreDestroy
-import javax.sql.DataSource
 
 @SpringBootApplication(
     // this makes scanning annotations in other packages work
